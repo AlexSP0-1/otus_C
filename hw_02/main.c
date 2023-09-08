54a6b6c8bc78acef731f46bde24f0da7aa1bf991
Alexey Saprunov <workernight@yandex.ru>
2023-09-08 21:07:27 +0400

    intial commit

---
+++ hw_02/main.c
@@ -0,0 +1,13 @@
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#include "charset.h"
+
+int main()
+{
+    printf("Hello World!\n");
+    return 0;
+}
