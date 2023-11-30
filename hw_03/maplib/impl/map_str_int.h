eeba3b618bb0b4ac67797defc514cc7322074247
Alexey Saprunov <workernight@yandex.ru>
2023-11-30 20:56:19 +0400

    chore: implement maplib

---
+++ hw_03/maplib/impl/map_str_int.h
@@ -0,0 +1,57 @@
+//Adaptation for map (CONST CHAR* -> INTMAX_Y)
+
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+
+#ifndef MAP_MAX_KEY_LENGTH
+#define MAP_MAX_KEY_LENGTH 8096
+#endif
+
+#if MAP_MAX_KEY_LENGTH < 2
+    #error Ivalid MAP_MAX_KEY_LENGTH
+#endif
+
+static bool map_key_compare(const char *ls, const char *rs)
+{
+    if( ls == rs)
+    {
+        return true;
+    }
+
+    if(ls == NULL || rs == NULL)
+        return false;
+
+    return strncmp(ls, rs, MAP_MAX_KEY_LENGTH) == 0;
+}
+
+static MapEntry *map_create_entry(const char *rs, intmax_t val)
+{
+    assert(rs != NULL);
+    size_t len = strnlen(rs, MAP_MAX_KEY_LENGTH);
+    char *pstr = (char*)malloc(len+1);
+    if(pstr == NULL)
+    {
+        return NULL;
+    }
+
+    memcpy(pstr, rs, len);
+    pstr[len] = '\0';
+
+    MapEntry *pe = (MapEntry *) malloc(sizeof(MapEntry));
+    if(pe == NULL)
+    {
+        free(pstr);
+        return NULL;
+    }
+    pe->key = pstr;
+    pe->val = val;
+    return pe;
+}
+
+static void map_free_entry(MapEntry *pe)
+{
+    assert(pe);
+    free((void *) pe->key);
+    free(pe);
+}
---
+++ hw_03/maplib/maplib.h
@@ -0,0 +1,64 @@
+#ifndef MAPLIB_H
+#define MAPLIB_H
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#ifndef MAP_TYPE
+#define MAP_TYPE STR_TO_INTMAX
+#endif
+
+#if MAP_TYPE == STR_TO_INTMAX
+typedef const char *MapKey;
+typedef intmax_t MapVal;
+#define MAP_MAX_KEY_LENGTH 8096
+#else
+#error INCORRECT MAP_TYPE!
+#endif
+
+typedef struct
+{
+    MapKey key;
+    MapVal val;
+} MapEntry;
+
+typedef uint32_t (*HashFunc)(MapKey);
+
+typedef struct Map Map;
+
+/// Create new map
+Map *map_new(HashFunc hash_func);
+
+/// Insert key-value; return false on error or if key already present
+bool map_insert(Map *map, MapKey key, MapVal val);
+
+/// Remove key-value; return false if key is no in map
+bool map_remove(Map *map, MapKey key);
+
+/// Find location of value associated with key;
+/// return NULL, if key is not in map
+MapVal *map_find(Map *map, MapKey key);
+
+/// Return number of elements, stored in map
+size_t map_count(Map *map);
+
+/// Free memory
+void map_destroy(Map **map);
+
+// Iterators
+
+typedef size_t MapIter;
+
+static inline MapIter map_iter_end()
+{
+    return SIZE_MAX;
+}
+
+MapIter map_iter_begin(Map *map);
+
+MapEntry *map_iter_get(Map *map, MapIter iter);
+
+MapIter map_iter_next(Map *map, MapIter iter);
+
+#endif
